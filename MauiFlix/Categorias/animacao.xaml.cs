21f51fdd8491e396d75431e47349e9e812217ad0
user <user@DESKTOP-AOMDCF4>
2024-06-03 19:11:24 -0300

    Adicionar arquivos de projeto.

---
+++ MauiFlix/Categorias/animacao.xaml.cs
@@ -0,0 +1,43 @@
+using MauiFlix.Filmes.animacao;
+
+namespace MauiFlix;
+
+public partial class animacao : ContentPage
+{
+	public animacao()
+	{
+		InitializeComponent();
+	}
+
+    private void ImageButton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void dragonbtn_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            Navigation.PushAsync(new dragon());
+        }
+        catch (Exception ex)
+        {
+            DisplayAlert("OPS!!", ex.Message, "OK");
+        }
+    }
+
+    private void spiderbutton_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void toystory_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void up_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
